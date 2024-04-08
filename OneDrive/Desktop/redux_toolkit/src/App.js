f65494ae7ef451c21db9407d061c731f9bde82a8
Akashanil-2000 <akash.sindhuanil@gmail.com>
2024-04-08 09:57:19 +0530

    first commit

---
+++ OneDrive/Desktop/redux_toolkit/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Initial value</h1>
+      <button>Increment</button>
+    </div>
+  );
+}
+
+export default App;
