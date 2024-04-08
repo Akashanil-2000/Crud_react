7c2c9a151be772445dac71cdcd48c0044d501286
Akashanil-2000 <akash.sindhuanil@gmail.com>
2024-04-08 10:17:20 +0530

    added increment and decremen

--- OneDrive/Desktop/redux_toolkit/src/App.js
+++ OneDrive/Desktop/redux_toolkit/src/App.js
@@ -1,10 +1,16 @@
 import './App.css';
+import {useSelector} from "react-redux"
+import {useDispatch} from "react-redux"
+import { decrement, increment } from './feature/counterSlice';
 
 function App() {
+  const {count1}=useSelector((state)=>state.counter)
+  const dispatch=useDispatch()
   return (
     <div className="App">
-      <h1>Initial value</h1>
-      <button>Increment</button>
+      <h1>Initial value:{count1}</h1>
+      <button onClick={()=>dispatch(increment())}>Increment</button>
+      <button onClick={()=>dispatch(decrement())}>Decrement</button>
     </div>
   );
 }
--- OneDrive/Desktop/redux_toolkit/src/feature/counterSlice.js
+++ OneDrive/Desktop/redux_toolkit/src/feature/counterSlice.js
@@ -1,17 +1,20 @@
 import { createSlice } from "@reduxjs/toolkit";
 
-const initialState=()=>{
+const initialState={
     count1:0
 }
 const counterSlice=createSlice({
     name:"counter",
     initialState,
     reducers:{
-        increment:(state,action)=>{
+        increment:(state)=>{
             state.count1=state.count1+1
+        },
+        decrement:(state)=>{
+            state.count1=state.count1-1
         }
     }
 })
 
 export default counterSlice.reducer
-export const {increment}=counterSlice.actions
+export const {increment,decrement}=counterSlice.actions
